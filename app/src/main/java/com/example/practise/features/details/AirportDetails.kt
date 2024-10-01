19a85c736437ef32c65f74f683e54e8a4b8786cf
Bala Dakshina <gdbala87@gmail.com>
2024-10-02 00:41:43 +0100

    align heading to the center of the screen

--- app/src/main/java/com/example/practise/features/details/AirportDetails.kt
+++ app/src/main/java/com/example/practise/features/details/AirportDetails.kt
@@ -1,12 +1,12 @@
 package com.example.practise.features.details
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,7 +18,7 @@ fun AirportDetailsScreen(viewModel: AirPortDetailsViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Airport Details", modifier = Modifier.fillMaxWidth())
+        Text(text = "Airport Details", modifier = Modifier.align(Alignment.CenterHorizontally))
         HorizontalDivider(modifier = Modifier.padding(4.dp))
         Text(text = "Country : ${state.country}")
         Text(text = "City : ${state.city}")

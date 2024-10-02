8aebbbbda0cfe2bc030d7f21b863789a790bd254
Bala Dakshina <gdbala87@gmail.com>
2024-10-02 22:32:04 +0100

    Add mappers and dimensions file

---
+++ lib-data/src/main/java/com/example/lib_data/mapper/AirPortListMapper.kt
@@ -0,0 +1,19 @@
+package com.example.lib_data.mapper
+
+import com.example.lib_data.model.AirPortListResponse
+import com.example.lib_domain.ResultType
+import com.example.lib_domain.asResult
+import com.example.lib_domain.model.AirPort
+import javax.inject.Inject
+
+class AirPortListMapper @Inject constructor() {
+    fun map(response: ResultType<AirPortListResponse>) =
+        response.asResult { result ->
+            result.data.map {
+                AirPort(
+                    name = it.attributes.name,
+                    id = it.id.orEmpty()
+                )
+            }
+        }
+}
